4a2792672ed4571c4db4d062316018c3782e2688
xaircore <xaircore@gmail.com>
2019-01-14 20:49:16 +0300

    Update MyArrayListTest.java

--- L3/src/test/java/com/xairlab/otus/collection/MyArrayListTest.java
+++ L3/src/test/java/com/xairlab/otus/collection/MyArrayListTest.java
@@ -1,4 +1,4 @@
-package test.java.com.xairlab.otus.collection;
+package com.xairlab.otus.collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
@@ -7,93 +7,116 @@ class MyArrayListTest {
 
     @Test
     void size() {
+        assertTrue(false);
     }
 
     @Test
     void isEmpty() {
+        assertTrue(false);
     }
 
     @Test
     void contains() {
+        assertTrue(false);
     }
 
     @Test
     void iterator() {
+        assertTrue(false);
     }
 
     @Test
     void toArray() {
+        assertTrue(false);
     }
 
     @Test
     void toArray1() {
+        assertTrue(false);
     }
 
     @Test
     void add() {
+        assertTrue(false);
     }
 
     @Test
     void remove() {
+        assertTrue(false);
     }
 
     @Test
     void containsAll() {
+        assertTrue(false);
     }
 
     @Test
     void addAll() {
+        assertTrue(false);
     }
 
     @Test
     void addAll1() {
+        assertTrue(false);
     }
 
     @Test
     void removeAll() {
+        assertTrue(false);
     }
 
     @Test
     void retainAll() {
+        assertTrue(false);
     }
 
     @Test
     void clear() {
+        assertTrue(false);
     }
 
     @Test
     void get() {
+        assertTrue(false);
     }
 
     @Test
     void set() {
+        assertTrue(false);
     }
 
     @Test
     void add1() {
+        assertTrue(false);
     }
 
     @Test
     void remove1() {
+        assertTrue(false);
     }
 
     @Test
     void indexOf() {
+        assertTrue(false);
     }
 
     @Test
     void lastIndexOf() {
+        assertTrue(false);
     }
 
     @Test
     void listIterator() {
+        assertTrue(false);
     }
 
     @Test
     void listIterator1() {
+        assertTrue(false);
     }
 
     @Test
     void subList() {
+        assertTrue(false);
     }
 }
