04ac6a067dbd49c5f845956ce91f49833269f536
xaircore <xaircore@gmail.com>
2019-01-14 20:45:38 +0300

    MyArrayList

---
+++ L3/src/main/java/com/xairlab/otus/collection/Entry.java
@@ -0,0 +1,7 @@
+package com.xairlab.otus.collection;
+
+public class Entry {
+    public static void main(String[] args){
+        System.out.println("Collection");
+    }
+}
---
+++ L3/src/test/java/com/xairlab/otus/collection/MyArrayListTest.java
@@ -0,0 +1,99 @@
+package test.java.com.xairlab.otus.collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class MyArrayListTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void iterator() {
+    }
+
+    @Test
+    void toArray() {
+    }
+
+    @Test
+    void toArray1() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void containsAll() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void addAll1() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void retainAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void add1() {
+    }
+
+    @Test
+    void remove1() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void lastIndexOf() {
+    }
+
+    @Test
+    void listIterator() {
+    }
+
+    @Test
+    void listIterator1() {
+    }
+
+    @Test
+    void subList() {
+    }
+}
