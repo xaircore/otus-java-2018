a4dc50cb15d102ebaff7cbd7c940714a77296ca2
xaircore <xaircore@gmail.com>
2019-01-20 19:58:02 +0300

    MyArrayList

---
+++ L3/src/test/java/com/xairlab/otus/collection/ProofMyArrayListTest.java
@@ -0,0 +1,23 @@
+package com.xairlab.otus.collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ProofMyArrayListTest {
+
+    @Test
+    void addAll() {
+        assertTrue(false);
+    }
+
+    @Test
+    void copy() {
+        assertTrue(false);
+    }
+
+    @Test
+    void sort() {
+        assertTrue(false);
+    }
+}
