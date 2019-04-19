9e55ee16b6ef7f972b665b6a45ca7bb921e4e76b
xaircore <xaircore@gmail.com>
2019-04-19 21:26:22 +0300

    atomic counter

--- L13/src/main/java/com/xairlab/otus/thread/Main.java
+++ L13/src/main/java/com/xairlab/otus/thread/Main.java
@@ -4,32 +4,28 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-    private int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    private int[] array2 = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
     private AtomicInteger counter = new AtomicInteger();
     int index = 0;
     boolean reverse = false;
 
     private void action(int current) {
         while (true) {
-            if (counter.compareAndSet(10, 0)) {
-                if (reverse) {
-                    Thread.currentThread().interrupt();
-                } else {
-                    array = array2;
-                    reverse = true;
-                    index = 0;
-                }
-            }
             if (counter.get() % 2 == current) {
-                if (counter.compareAndSet(index + 1, index)) {
-                    System.out.println(Thread.currentThread().getName() + " : " + array[index]);
-                    index = counter.incrementAndGet();
-                } else {
-                    System.out.println(Thread.currentThread().getName() + " : " + array[index]);
-                    counter.incrementAndGet();
+                if (counter.get() % 18 == 1) {
+                    reverse = !reverse;
                 }
                 try {
+                    System.out.println(Thread.currentThread().getName() + " : " + index);
+                    if (reverse) {
+                        if (current == 1) {
+                            index++;
+                        }
+                    } else {
+                        if (current == 1) {
+                            index--;
+                        }
+                    }
+                    counter.getAndIncrement();
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     break;
